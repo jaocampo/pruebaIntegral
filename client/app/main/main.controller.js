f01e0d9b542676e56f7915006752870ba21a8f58
jonathanrios3 <you@example.com>
2017-12-05 11:47:32 -0500

    Footer

--- client/app/main/main.controller.js
+++ client/app/main/main.controller.js
@@ -4,18 +4,45 @@
 
   class MainController {
 
-    constructor($http) {
-      this.$http = $http;
-      this.awesomeThings = [];
-    }
+  constructor() {
 
-    $onInit() {
-      this.$http.get('/api/things')
-        .then(response => {
-          this.awesomeThings = response.data;
-        });
-    }
   }
+
+  $onInit() {
+    //function to animate slider for page landing
+function doAnimations(elems){
+  //Cache the animationend event in a variable
+  var animEndEv ='webkitAnimationEnd animationend';
+
+  elems.each(function(){
+    var $this = $(this),
+    $animationType =$this.data('animation');
+  $this.addClass($animationType).one(animEndEv,function(){
+    $this.removeClass($animationType);
+  });
+});
+}
+//variables on page load
+var $myCarousel = $('#prodructosCarousel'),
+$firstAnimatingElems =$myCarousel.find('.item:first').find("[data-animation ^= 'animated']");
+
+//Initialize carousel
+$myCarousel.carousel();
+
+//animated captions in first slide on page load
+doAnimations($firstAnimatingElems);
+
+//Pause carousel
+//$myCarousel.carousel('Pause');
+
+//others slides to be animated on carousel slide event
+$myCarousel.on('slide.bs.carousel',function(e){
+  var $animatingElems = $(e.relatedTarget).find("[data-animation ^='animated']");
+  doAnimations($animatingElems);
+});
+
+  }
+}
 
   angular.module('paraisoCiclistaApp')
     .component('main', {
